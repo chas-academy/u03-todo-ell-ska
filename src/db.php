efca013b61c33120c36e5e9b5a5b86cb50096927
ell-ska <ella@skagerstrom.se>
2024-12-05 15:18:33 +0100

    feat: create database class

---
+++ src/db.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+  private static ?PDO $instance = null;
+
+  public static function getInstance(): PDO {
+    if (self::$instance === null) {
+      $host = 'mariadb';
+      $dbname = 'todo';
+      $username = 'mariadb';
+      $password = 'mariadb';
+
+      $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      ];
+
+      try {
+        self::$instance = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password, $options);
+      } catch (PDOException $error) {
+        die($error->getMessage());
+      }
+    }
+
+    return self::$instance;
+  }
+}
