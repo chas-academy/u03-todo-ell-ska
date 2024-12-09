03b5f8c6e71a4d3a0928ca16fe123b8bef28dfd2
ell-ska <ella@skagerstrom.se>
2024-12-09 17:35:53 +0100

    feat: add layout

--- src/index.php
+++ src/index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <link rel="stylesheet" href="./styles/style.css" />
-</head>
-
-<body>
-  <?php require_once './components/sidebar.php' ?>
-</body>
-
-</html>
+<?php
+$title = 'Inbox';
+ob_start();
+?>
+<?php require_once './components/sidebar.php' ?>
+<?php
+$content = ob_get_clean();
+require 'layout.php';
