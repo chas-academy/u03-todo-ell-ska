42d59aa86a12b36dc4c2a9022986e5a4d9b5a093
ell-ska <ella@skagerstrom.se>
2024-12-11 12:50:37 +0100

    feat: create log in page

---
+++ src/log-in.php
@@ -0,0 +1,27 @@
+<?php
+$title = 'Log in';
+ob_start();
+?>
+
+<main class="auth">
+  <section>
+    <h1>Welcome back!</h1>
+    <form method="post">
+      <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" placeholder="Enter your username">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Enter your password">
+      </div>
+      <button type="submit">Log in</button>
+    </form>
+  </section>
+  <a href="/log-in.php">Don't have an account? Sign up.</a>
+</main>
+
+<?php
+$content = ob_get_clean();
+require 'layout.php'
+?>
