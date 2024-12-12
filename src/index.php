65dc098ef604dc3bafd28171fe66534e890d8dfc
ell-ska <ella@skagerstrom.se>
2024-12-12 13:23:18 +0100

    render inbox tasks

--- src/index.php
+++ src/index.php
@@ -1,12 +1,21 @@
 <?php
+require_once 'db.php';
 require_once 'lib/auth.php';
 require_once 'utils/redirect.php';
 require_once 'utils/session-start-unless-started.php';
 
 sessionStartUnlessStarted();
-if (!Auth::getUser()) {
+$user = Auth::getUser();
+
+if (!$user) {
   redirect('/log-in.php');
 }
+
+$db = Database::getInstance();
+
+$query = $db->prepare("SELECT * FROM tasks WHERE user_id = :id AND list_id IS NULL");
+$query->execute(['id' => $user['id']]);
+$tasks = $query->fetchAll();
 
 $title = 'Inbox';
 ob_start();
@@ -25,30 +34,38 @@ ob_start();
   </header>
 
   <ul>
-    <li class="task">
-      <div class="main">
-        <div class="checkbox">
-          <label>done</label>
-          <input type="checkbox">
-          <?php
-          require_once 'components/icons/check.php';
+    <?php foreach ($tasks as $task) : ?>
+      <li class="task">
+        <div class="main">
+          <div class="checkbox">
+            <label for="done">task finished</label>
+            <input type="checkbox" name="done" id="done" <?= $task['done'] ? 'checked' : '' ?>>
+            <?php
+            require_once 'components/icons/check.php';
 
-          $icon = new Check(12);
-          $icon->render();
-          ?>
+            $icon = new Check(12);
+            $icon->render();
+            ?>
+          </div>
+          <div class="text">
+            <?php if (isset($task['when'])) : ?>
+              <time><?= $task['when'] ?></time>
+            <?php endif ?>
+            <h2><?= $task['name'] ?></h2>
+            <?php if (isset($task['list'])) : ?>
+              <span><?= $task['list'] ?></span>
+            <?php endif ?>
+          </div>
         </div>
-        <div>
-          <time>when</time>
-          <h2>label</h2>
-          <span>list</span>
-        </div>
-      </div>
-      <div class="deadline">
-        <img src="assets/icons/flag.svg" alt="flag icon" width="12" height="12" />
-        <span>due:</span>
-        <time>deadline</time>
-      </div>
-    </li>
+        <?php if (isset($task['deadline'])) : ?>
+          <div class="deadline">
+            <img src="assets/icons/flag.svg" alt="flag icon" width="12" height="12" />
+            <span>due:</span>
+            <time><?= $task['deadline'] ?></time>
+          </div>
+        <?php endif ?>
+      </li>
+    <?php endforeach; ?>
   </ul>
 </main>
 
