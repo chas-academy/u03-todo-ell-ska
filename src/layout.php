8799cfd4b861a5b4a5781efff6dfafa1e1be576f
ell-ska <ella@skagerstrom.se>
2025-01-06 19:51:54 +0100

    feat: add favicon

--- src/layout.php
+++ src/layout.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?= $title ? "$title | Ordna" : 'Ordna' ?></title>
+  <link rel="icon" href="favicon.svg">
   <link rel="stylesheet" href="./styles/style.css" />
   <script src="scripts/modal.js" defer></script>
   <script src="scripts/input-preview.js" defer></script>
