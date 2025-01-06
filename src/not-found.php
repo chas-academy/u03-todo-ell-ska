2fd46c2f35702f78aa462253ea643f6a5b0373b6
ell-ska <ella@skagerstrom.se>
2025-01-06 19:42:57 +0100

    feat: create not found page

---
+++ src/not-found.php
@@ -0,0 +1,16 @@
+<?php
+$title = 'Page not found';
+ob_start();
+?>
+
+<main class="not-found">
+  <div>
+    <h1>404</h1>
+    <p>Oops! The page you are looking for can't be found.</p>
+    <a href="/">Go back home</a>
+  </div>
+</main>
+
+<?php
+$content = ob_get_clean();
+require_once __DIR__ . '/layout.php';
