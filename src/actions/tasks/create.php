35a28f9e47a3dae98cd5c0f41a80cd7a86fbc701
ell-ska <ella@skagerstrom.se>
2025-01-07 14:32:30 +0100

    feat: prefill task

--- src/actions/tasks/create.php
+++ src/actions/tasks/create.php
@@ -4,12 +4,13 @@ require_once __DIR__ . '/../../lib/auth.php';
 require_once __DIR__ . '/../../utils/navigation.php';
 require_once __DIR__ . '/../../utils/validation.php';
 
-function createTask($name, $note, $deadline, $scheduled, $listId) {
+function createTask($name, $note, $deadline, $scheduled, $listId, $done) {
   $name = validateString($name, true, 'Task name is required');
   $note = validateString($note);
   $deadline = validateString($deadline);;
   $scheduled = validateString($scheduled);
   $listId = validateString($listId);
+  $done = validateString($done) ?? '0';
 
   $db = Database::getInstance();
   $user = Auth::getUser();
@@ -19,8 +20,8 @@ function createTask($name, $note, $deadline, $scheduled, $listId) {
   }
 
   try {
-    $query = $db->prepare('INSERT INTO tasks (name, note, deadline, scheduled, list_id, user_id) VALUES (:name, :note, :deadline, :scheduled, :listId, :userId)');
-    $query->execute(['name' => $name, 'note' => $note, 'deadline' => $deadline, 'scheduled' => $scheduled, 'listId' => $listId, 'userId' => $user['id']]);
+    $query = $db->prepare('INSERT INTO tasks (name, note, deadline, scheduled, done, list_id, user_id) VALUES (:name, :note, :deadline, :scheduled, :done, :listId, :userId)');
+    $query->execute(['name' => $name, 'note' => $note, 'deadline' => $deadline, 'scheduled' => $scheduled, 'done' => $done, 'listId' => $listId, 'userId' => $user['id']]);
 
     refresh();
   } catch (PDOException $error) {
