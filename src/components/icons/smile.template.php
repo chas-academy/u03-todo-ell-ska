168c3a3be91d86e4814d86ff823d331621a79f35
ell-ska <ella@skagerstrom.se>
2025-01-07 13:32:37 +0100

    feat: list empty state

---
+++ src/components/icons/smile.template.php
@@ -0,0 +1,14 @@
+<svg
+  width="<?= $this->size ?>"
+  height="<?= $this->size ?>"
+  viewBox="0 0 12 12"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M4 7C4 7 4.75 8 6 8C7.25 8 8 7 8 7M4.5 4.5H4.505M7.5 4.5H7.505M11 6C11 8.76142 8.76142 11 6 11C3.23858 11 1 8.76142 1 6C1 3.23858 3.23858 1 6 1C8.76142 1 11 3.23858 11 6Z"
+    stroke="currentColor"
+    stroke-width="<?= $this->strokeWidth ?>"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    vector-effect="non-scaling-stroke" />
+</svg>
--- src/components/task-list.template.php
+++ src/components/task-list.template.php
@@ -1,21 +1,32 @@
-<ul>
-  <?php
-  foreach ($this->tasks as $task) {
-    require __DIR__ . '/task-list-item.template.php';
-  }
-  ?>
-  <?php if ($this->overdueTasks) : ?>
-    <div class="separator">
-      <div></div>
-      <p>Overdue</p>
-      <div></div>
-    </div>
+<?php require_once __DIR__ . '/icon.php' ?>
 
-  <?php
-    foreach ($this->overdueTasks as $task) {
+<?php if ((isset($this->tasks) && count($this->tasks) !== 0) || (isset($this->overdueTasks) && count($this->overdueTasks))) : ?>
+  <ul>
+    <?php
+    foreach ($this->tasks as $task) {
       require __DIR__ . '/task-list-item.template.php';
     }
+    ?>
+    <?php if ($this->overdueTasks) : ?>
+      <div class="separator">
+        <div></div>
+        <p>Overdue</p>
+        <div></div>
+      </div>
+    <?php
+      foreach ($this->overdueTasks as $task) {
+        require __DIR__ . '/task-list-item.template.php';
+      }
 
-  endif;
-  ?>
-</ul>
+    endif;
+    ?>
+  </ul>
+<?php else :  ?>
+  <div class="empty">
+    <h2>All done!</h2>
+    <div>
+      <p>Enjoy your day</p>
+      <?php Icon::render('smile', 16) ?>
+    </div>
+  </div>
+<?php endif ?>
