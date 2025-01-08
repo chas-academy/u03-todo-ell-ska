ecaac6fbbf78107735bedc4317061c7ffbc2c72d
ell-ska <ella@skagerstrom.se>
2025-01-08 15:44:53 +0100

    feat: profile menu styling

--- src/components/profile-menu.php
+++ src/components/profile-menu.php
@@ -1,1 +1,14 @@
-<div id="<?= $this->location ?>-profile-menu" class="profile-menu hidden">profile menu</div>
+<?php require_once __DIR__ . '/icon.php' ?>
+
+<div id="<?= $this->location ?>-profile-menu" class="profile-menu hidden">
+  <form action="" method="post">
+    <button>
+      <?php Icon::render('log-out', 16) ?>
+      <span>Log out</span>
+    </button>
+    <button>
+      <?php Icon::render('circle-x', 16) ?>
+      <span>Delete account</span>
+    </button>
+  </form>
+</div>
